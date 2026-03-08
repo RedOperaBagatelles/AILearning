6f2bd12b0a0a7209508085fe11e0e2cc4b3319dc
RedOpera <showme.the1018@gmail.com>
2026-03-08 18:48:26 +0900

    Tensorflow, CUDA 설치

---
+++ AILearning.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <External/tensorflow/include/c/c_api.h>
+#include <windows.h>
+
+#pragma comment(lib, "External/lib/tensorflow.lib")
+
+int main()
+{
+	printf("Hello from TensorFlow C library version %s\n", TF_Version());
+
+	return 0;
+}
